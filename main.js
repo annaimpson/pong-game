bf133f22a495b32bd61880f6f696ef99b447da82
Anna Impson <annaimpson@gmail.com>
2017-12-05 14:09:34 -0500

    changed some naming

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var pongOne = document.getElementById('pong1');
 var pongTwo = document.getElementById('pong2');
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var paddleHeight = 150;
-var paddleWidth = 8;
+var pongHeight = 150;
+var pongWidth = 8;
 var ballRadius = 3;
-var speedOfPaddle1 = 0;
-var positionOfPaddle1 = 450;
-var speedOfPaddle2 = 0;
-var positionOfPaddle2 = 450;
+var speedOfPong1 = 0;
+var positionOfPong1 = 450;
+var speedOfPong2 = 0;
+var positionOfPong2 = 450;
 var x = 100;
 var y = 100;
 var drawX = 1;
@@ -19,38 +19,38 @@ var drawY = -1;
 function pongGame(){
     document.addEventListener('keydown', function (e) {
          if (e.keyCode == 87) { //W
-            positionOfPaddle1 -= 25;
-            pongOne.style.top = (positionOfPaddle1) + "px";
+            positionOfPong1 -= 25;
+            pongOne.style.top = (positionOfPong1) + "px";
          }
          if (e.keyCode == 83) { //S
-            positionOfPaddle1 += 25;
-            pongOne.style.top = (positionOfPaddle1) + "px";
+            positionOfPong1 += 25;
+            pongOne.style.top = (positionOfPong1) + "px";
          }
          if (e.keyCode == 38) { //up arrow
-            positionOfPaddle2 -= 25;
-            pongTwo.style.top = (positionOfPaddle2) + "px";
+            positionOfPong2 -= 25;
+            pongTwo.style.top = (positionOfPong2) + "px";
          }
          if (e.keyCode == 40) { //down arrow
-            positionOfPaddle2 += 25;
-            pongTwo.style.top = (positionOfPaddle2) + "px";
+            positionOfPong2 += 25;
+            pongTwo.style.top = (positionOfPong2) + "px";
          }
     }, false);
 
     window.setInterval(function show() {
-        positionOfPaddle1 += speedOfPaddle1;
-        positionOfPaddle2 += speedOfPaddle2;
+        positionOfPong1 += speedOfPong1;
+        positionOfPong2 += speedOfPong2;
 
-        if (positionOfPaddle1 <= 30) {
-            positionOfPaddle1 = 30;
+        if (positionOfPong1 <= 30) {
+            positionOfPong1 = 30;
         }
-        if (positionOfPaddle2 <= 30) {
-            positionOfPaddle2 = 30;
+        if (positionOfPong2 <= 30) {
+            positionOfPong2 = 30;
         }
-        if (positionOfPaddle1 >= window.innerHeight - paddleHeight) {
-            positionOfPaddle1 = window.innerHeight - paddleHeight;
+        if (positionOfPong1 >= window.innerHeight - pongHeight) {
+            positionOfPong1 = window.innerHeight - pongHeight;
         }
-        if (positionOfPaddle2 > window.innerHeight - paddleHeight) {
-            positionOfPaddle2 = window.innerHeight - paddleHeight;
+        if (positionOfPong2 > window.innerHeight - pongHeight) {
+            positionOfPong2 = window.innerHeight - pongHeight;
         }
 
     }, 1000/60);
@@ -69,11 +69,11 @@ function pongGame(){
         if(y + drawY > canvas.height - ballRadius || y + drawY < ballRadius) {
             drawY = -drawY;
         }
-        if(x + drawX > canvas.width - ballRadius || x + drawX < ballRadius) {
-            drawX = -drawX;
-            alert("GAME OVER");
-            document.location.reload();
-        }
+        // if(x + drawX > canvas.width - ballRadius || x + drawX < ballRadius) {
+        //     drawX = -drawX;
+        //     alert("GAME OVER");
+        //     document.location.reload();
+        // }
         x += drawX;
         y += drawY;
     }
