edc1ce3e8d2040de379f12b8c082b27ea1f59056
Anna Impson <annaimpson@gmail.com>
2017-12-12 13:52:05 -0500

    changed how the game was set up. began rework

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-var pongOne = document.getElementById('pong1');
-var pongTwo = document.getElementById('pong2');
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var pongHeight = 150;
-var pongWidth = 8;
-var ballRadius = 3;
-var speedOfPong1 = 0;
-var positionOfPong1 = 450;
-var speedOfPong2 = 0;
-var positionOfPong2 = 450;
+var pongHeight = 100;
+var pongWidth = 10;
+var pongPositionOne = 250;
+var pongPositionTwo = 250;
+var ballRadius = 10;
 var x = 100;
 var y = 100;
 var drawX = 1;
 var drawY = -1;
-// var score1;
-// var score2;
+var paddleX = (context.width - pongWidth)/2;
+console.log(canvas.width);
 
 function pongGame(){
     document.addEventListener('keydown', function (e) {
@@ -36,47 +32,82 @@ function pongGame(){
          }
     }, false);
 
-    window.setInterval(function show() {
-        positionOfPong1 += speedOfPong1;
-        positionOfPong2 += speedOfPong2;
-
-        if (positionOfPong1 <= 30) {
-            positionOfPong1 = 30;
-        }
-        if (positionOfPong2 <= 30) {
-            positionOfPong2 = 30;
-        }
-        if (positionOfPong1 >= window.innerHeight - pongHeight) {
-            positionOfPong1 = window.innerHeight - pongHeight;
-        }
-        if (positionOfPong2 > window.innerHeight - pongHeight) {
-            positionOfPong2 = window.innerHeight - pongHeight;
-        }
-
-    }, 1000/60);
-
     function drawBall() {
         context.beginPath();
-        context.arc(x, y, ballRadius, 0, Math.PI*2);
+        context.arc(x, y, ballRadius, 0, 2 * Math.PI); //x coordinate, y coordinate, radius, starting angle, ending angle
         context.fillStyle = "#efdce9";
         context.fill();
         context.closePath();
     }
 
-    function draw() {
+    function drawRightPaddle() {
+        context.beginPath();
+        context.lineWidth = "3";
+        context.strokeStyle = "#efdce9";
+        context.fillStyle = "#efdce9";
+        context.rect(0, pongPositionOne, pongWidth, pongHeight);
+        context.fill();
+        context.stroke();
+        document.addEventListener('keydown', function (e) {
+             if (e.keyCode == 87) { //W
+                pongPositionOne -= 0.1;
+             }
+             if (e.keyCode == 83) { //S
+                pongPositionOne += 0.1;
+             }
+        }, false);
+    }
+
+    function drawLeftPaddle() {
+        context.beginPath();
+        context.lineWidth = "3";
+        context.strokeStyle = "#efdce9";
+        context.fillStyle = "#efdce9";
+        context.rect(canvas.height - pongWidth, pongPositionTwo, pongWidth, pongHeight);
+        context.fill();
+        context.stroke();
+        document.addEventListener('keydown', function (e) {
+             if (e.keyCode == 38) { //up arrow
+                pongPositionTwo -= 0.1;
+             }
+             if (e.keyCode == 40) { //down arrow
+                pongPositionTwo += 0.1;
+             }
+        }, false);
+    }
+
+    window.setInterval(function show() {
         context.clearRect(0, 0, canvas.width, canvas.height); //clears the ball trail
         drawBall();
-        if(y + drawY > canvas.height - ballRadius || y + drawY < ballRadius) {
+        drawRightPaddle();
+        drawLeftPaddle();
+
+        if (pongPositionOne <= 0 || pongPositionOne >= canvas.height - pongHeight) { //keeps left bar from going beyond top or bottom bar
+            pongPositionOne = 0;
+            pongPositionOne = window.innerHeight - pongHeight;
+        }
+        // if (positionOfPong2 <= 30 || positionOfPong2 > window.innerHeight - pongHeight) { //keeps right bar from going beyond top or bottom bar
+        //     positionOfPong2 = 30;
+        //     positionOfPong2 = window.innerHeight - pongHeight;
+        // }
+        if(y + drawY > canvas.height || y + drawY < 0) { //bounces the ball off the top and bottom of screen
             drawY = -drawY;
         }
-        // if(x + drawX > canvas.width - ballRadius || x + drawX < ballRadius) {
-        //     drawX = -drawX;
-        //     alert("GAME OVER");
-        //     document.location.reload();
-        // }
+        if(x + drawX < ballRadius) {
+            drawX = -drawX;
+        }
+        else if(x + drawX > canvas.width - ballRadius) { //TODO
+            if(x > paddleX && x < paddleX + pongWidth) {
+                drawX = -drawX;
+            }
+            else {
+                document.location.reload(); //restarts the game if the ball doesn't get hit
+            }
+        }
+
         x += drawX;
         y += drawY;
-    }
-    setInterval(draw, 10);
+
+    }, 1000/60);
 
 } pongGame();
